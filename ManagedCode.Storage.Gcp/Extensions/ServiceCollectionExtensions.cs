314673f43f4022366f0e9210e2142900dcb20a9b
Vitaliy Basanets <vitaliyb@managed-code.com>
2022-03-18 19:50:29 +0200

    added extention method

--- ManagedCode.Storage.Gcp/Extensions/ServiceCollectionExtensions.cs
+++ ManagedCode.Storage.Gcp/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using Google.Apis.Auth.OAuth2;
+using ManagedCode.Storage.Core;
 using ManagedCode.Storage.Gcp.Options;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -29,5 +30,26 @@ public static class ServiceCollectionExtensions
 
         return serviceCollection
             .AddScoped<IGCPStorage>(_ => new GCPStorage(gcpStorageOptions));
+    }
+
+    public static IServiceCollection AddGCPStorageAsDefault(
+        this IServiceCollection serviceCollection,
+        Action<GCPStorageOptions> action)
+    {
+        var gcpStorageOptions = new GCPStorageOptions();
+        action.Invoke(gcpStorageOptions);
+
+        var path = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            gcpStorageOptions.AuthFileName
+        );
+
+        using (Stream m = new FileStream(path, FileMode.Open))
+        {
+            gcpStorageOptions.GoogleCredential = GoogleCredential.FromStream(m);
+        }
+
+        return serviceCollection
+            .AddScoped<IStorage>(_ => new GCPStorage(gcpStorageOptions));
     }
 }
