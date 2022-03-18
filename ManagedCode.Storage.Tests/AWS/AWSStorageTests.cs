da805bc1efa5af8a264da274365e771aa4b251bc
ksemenenko <mail@ksemenenko.com>
2022-03-18 20:00:34 +0100

    path

--- ManagedCode.Storage.Tests/AWS/AWSStorageTests.cs
+++ ManagedCode.Storage.Tests/AWS/AWSStorageTests.cs
@@ -25,7 +25,7 @@ public class AWSStorageTests : StorageBaseTests
             opt.Bucket = "my-docs-1";
             opt.OriginalOptions = new AmazonS3Config
             {
-                ServiceURL = "http://myapp.s3:4566",
+                ServiceURL = "http://localhost:4566",
                 RegionEndpoint = RegionEndpoint.EUWest1,
                 ForcePathStyle = true,
                 UseHttp = true,
