2696a09b1e13b2a6cdd12cbd5f621bf06a7a09e0
ksemenenko <mail@ksemenenko.com>
2022-03-19 11:51:20 +0100

    tests

--- ManagedCode.Storage.Tests/Azure/AzureStorageTests.cs
+++ ManagedCode.Storage.Tests/Azure/AzureStorageTests.cs
@@ -22,7 +22,7 @@ public class AzureStorageTests : StorageBaseTests
             opt.Container = "documents";
             //https://github.com/marketplace/actions/azuright
             opt.ConnectionString =
-                "DefaultEndpointsProtocol=http; AccountName=devstoreaccount1; AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==; BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
+                "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1; AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
         });
 
         var provider = services.BuildServiceProvider();
