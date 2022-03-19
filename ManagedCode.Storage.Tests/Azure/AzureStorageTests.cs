838dd4ce3af35e30bac482f6369d39e8b1cac293
Vitaliy Basanets <vitaliyb@managed-code.com>
2022-03-19 17:03:55 +0200

    quick fix

--- ManagedCode.Storage.Tests/Azure/AzureStorageTests.cs
+++ ManagedCode.Storage.Tests/Azure/AzureStorageTests.cs
@@ -23,7 +23,7 @@ public class AzureStorageTests : StorageBaseTests
             opt.Container = "managed-code-bucket";
             //https://github.com/marketplace/actions/azuright
             opt.ConnectionString =
-                "DefaultEndpointsProtocol=https;AccountName=winktdev;AccountKey=F7F9vhS+SxgY8b0/mrGYZCV6QOoKwv8FqAHsDN/aZC4OPeyPhHS8OKRi3Uc9VIHcel5+oweEmRQs4Be+r0pFMg==;EndpointSuffix=core.windows.net";
+                "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;QueueEndpoint=http://localhost:10001/devstoreaccount1;TableEndpoint=http://localhost:10002/devstoreaccount1;";
         });
 
         var provider = services.BuildServiceProvider();
