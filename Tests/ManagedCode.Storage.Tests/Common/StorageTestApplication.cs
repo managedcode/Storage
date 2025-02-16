c38c42163d12a891941263e78b9317d6a9c2da69
ksemenenko <mail@ksemenenko.com>
2025-02-16 18:29:11 +0100

    tests

--- Tests/ManagedCode.Storage.Tests/Common/StorageTestApplication.cs
+++ Tests/ManagedCode.Storage.Tests/Common/StorageTestApplication.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Amazon.S3;
 using ManagedCode.Storage.Azure.Extensions;
 using ManagedCode.Storage.Azure.Options;
+using ManagedCode.Storage.Core.Extensions;
 using ManagedCode.Storage.FileSystem.Extensions;
 using ManagedCode.Storage.FileSystem.Options;
+using ManagedCode.Storage.Aws.Extensions;
+using ManagedCode.Storage.Aws.Options;
 using ManagedCode.Storage.Tests.Common.TestApp;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Hosting;
 using Testcontainers.Azurite;
+using Testcontainers.LocalStack;
+using Google.Cloud.Storage.V1;
+using ManagedCode.Storage.Google.Extensions;
+using ManagedCode.Storage.Google.Options;
+using Testcontainers.FakeGcsServer;
 using Xunit;
 
 namespace ManagedCode.Storage.Tests.Common;
@@ -17,30 +26,40 @@ namespace ManagedCode.Storage.Tests.Common;
 public class StorageTestApplication : WebApplicationFactory<HttpHostProgram>, ICollectionFixture<StorageTestApplication>
 {
     private readonly AzuriteContainer _azuriteContainer;
+    private readonly LocalStackContainer _localStackContainer;
+    private readonly FakeGcsServerContainer _gcpContainer;
 
     public StorageTestApplication()
     {
-        _azuriteContainer = new AzuriteBuilder().WithImage(ContainerImages.Azurite)
+        _azuriteContainer = new AzuriteBuilder()
+            .WithImage(ContainerImages.Azurite)
             .Build();
 
-        _azuriteContainer.StartAsync()
-            .Wait();
+        _localStackContainer = new LocalStackBuilder()
+            .WithImage(ContainerImages.LocalStack)
+            .Build();
+
+        _gcpContainer = new FakeGcsServerBuilder()
+            .WithImage(ContainerImages.FakeGCSServer)
+            .Build();
+
+        Task.WaitAll(
+            _azuriteContainer.StartAsync(),
+            _localStackContainer.StartAsync(),
+            _gcpContainer.StartAsync()
+        );
     }
 
     protected override IHost CreateHost(IHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            #region Add FileSystemStorage
+            services.AddStorageFactory();
 
             services.AddFileSystemStorage(new FileSystemStorageOptions
             {
                 BaseFolder = Path.Combine(Environment.CurrentDirectory, "managed-code-bucket")
             });
-
-            #endregion
-
-            #region Add AzureStorage
 
             services.AddAzureStorage(new AzureStorageOptions
             {
@@ -48,7 +67,32 @@ public class StorageTestApplication : WebApplicationFactory<HttpHostProgram>, IC
                 ConnectionString = _azuriteContainer.GetConnectionString()
             });
 
-            #endregion
+            services.AddGCPStorage(new GCPStorageOptions
+            {
+                BucketOptions = new BucketOptions
+                {
+                    ProjectId = "api-project-0000000000000",
+                    Bucket = "managed-code-bucket"
+                },
+                StorageClientBuilder = new StorageClientBuilder
+                {
+                    UnauthenticatedAccess = true,
+                    BaseUri = _gcpContainer.GetConnectionString()
+                }
+            });
+
+            
+            var config = new AmazonS3Config();
+            config.ServiceURL = _localStackContainer.GetConnectionString();
+            
+            services.AddAWSStorage(new AWSStorageOptions
+            {
+                PublicKey = "localkey",
+                SecretKey = "localsecret",
+                Bucket = "managed-code-bucket",
+                OriginalOptions = config
+            });
+
         });
 
         return base.CreateHost(builder);
@@ -56,6 +100,10 @@ public class StorageTestApplication : WebApplicationFactory<HttpHostProgram>, IC
 
     public override async ValueTask DisposeAsync()
     {
-        await _azuriteContainer.DisposeAsync();
+        await Task.WhenAll(
+            _azuriteContainer.DisposeAsync().AsTask(),
+            _localStackContainer.DisposeAsync().AsTask(),
+            _gcpContainer.DisposeAsync().AsTask()
+        );
     }
 }
